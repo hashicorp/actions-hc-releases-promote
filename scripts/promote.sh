169eb963ab0a47e73279633a858b091b7456e1a7
Jeanne Franco <jeanne.franco@hashicorp.com>
2022-03-29 13:45:11 -0700

    Handle Terraform sync registry

--- scripts/promote.sh
+++ scripts/promote.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
+#take this out when ready to PR
+set -x
+
 VERSION=${VERSION//v}
 
-hc-releases promote -product "$PRODUCT_NAME" \
--version "$VERSION" -source-env "$SOURCE_ENV"
+if [ "${SKIP_TERRAFORM_SYNC}" == "true" ]; then
+    hc-releases promote -product "$PRODUCT_NAME" \
+    -version "$VERSION" -source-env "$SOURCE_ENV" \
+    -skip-terraform-registry-sync
+else
+    hc-releases promote -product "$PRODUCT_NAME" \
+    -version "$VERSION" -source-env "$SOURCE_ENV"
+fi
